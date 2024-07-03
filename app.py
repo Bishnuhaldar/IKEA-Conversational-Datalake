4d28e19bd002ee976f0f1c5c547aa9ffc45855fd
Bishnuhaldar <109227807+Bishnuhaldar@users.noreply.github.com>
2024-07-03 13:37:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,7 @@ generation_config = {
 service_account_info = st.secrets["GCP_SERVICE_ACCOUNT_KEY"]
 credentials = service_account.Credentials.from_service_account_info(service_account_info)
 
-# # Load credentials from service account file
-# credentials = service_account.Credentials.from_service_account_file('data-driven-cx.json')
+
 
 # Set up BigQuery client
 project_id = 'data-driven-cx'
